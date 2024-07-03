b9924cff1b0c147410b68db1e6a352e78859be39
zen <zhangyiming748@gmail.com>
2024-07-03 20:01:22 +0800

    命令执行后一次性输出

--- util/cmd.go
+++ util/cmd.go
@@ -1,44 +1,23 @@
 package util
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 /*
 执行命令过程中可以循环打印消息
 */
 func ExecCommand(c *exec.Cmd, msg string) (e error) {
-	log.Printf("开始执行命令:%v\n", c.String())
+	log.Printf("%v:开始执行命令:%v\n", msg, c.String())
 
-	stdout, err := c.StdoutPipe()
-	c.Stderr = c.Stdout
+	output, err := c.CombinedOutput()
 	if err != nil {
-		log.Printf("连接Stdout产生错误:%v\t命令:%v\n", err, c.String())
-		return err
+		log.Fatalf("命令运行出现错误:%v\n", err)
+	} else {
+		log.Println(string(output))
 	}
-	if err = c.Start(); err != nil {
-		log.Printf("启动cmd命令产生错误:%v\t命令:%v\n", err, c.String())
-		return err
-	}
-	for {
-		tmp := make([]byte, 1024)
-		_, err := stdout.Read(tmp)
-		t := string(tmp)
-		t = strings.Replace(t, "\u0000", "", -1)
-		fmt.Printf("%v\n%v\n", t, msg)
-		if err != nil {
-			break
-		}
-	}
-	if err = c.Wait(); err != nil {
-		log.Printf("命令执行中产生错误:%v\t命令:%v\n", err, c.String())
-		return err
-	}
-
 	if isExitLabel() {
 		log.Printf("命令端获取到退出状态,命令结束后退出:%v\n", c.String())
 		os.Exit(0)
