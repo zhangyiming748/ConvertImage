13f8ea6ac39133a2a319cc196556b14883158b9c
zen <zhangyiming748@gmail.com>
2024-07-03 10:28:24 +0800

    添加crf限制

--- conv/avif.go
+++ conv/avif.go
@@ -1,23 +1,30 @@
 package conv
 
 import (
-	"github.com/zhangyiming748/ConvertImage/mediainfo"
 	"github.com/zhangyiming748/ConvertImage/replace"
 	"github.com/zhangyiming748/ConvertImage/util"
+	"github.com/zhangyiming748/FastMediaInfo"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
 /*
 转换一张图片为AVIF
 */
-func ProcessImage(in mediainfo.BasicInfo) {
+func ProcessImage(in util.BasicInfo) {
 	cleanName := replace.ForFileName(in.PurgeName)
 	out := strings.Join([]string{in.PurgePath, string(os.PathSeparator), cleanName, ".avif"}, "")
-
-	cmd := exec.Command("ffmpeg", "-y", "-i", in.FullPath, "-c:v", "libaom-av1", "-still-picture", "1", out)
+	mi := FastMediaInfo.GetStandMediaInfo(in.FullPath)
+	width, _ := strconv.Atoi(mi.Image.Width)
+	height, _ := strconv.Atoi(mi.Image.Height)
+	crf := FastMediaInfo.GetCRF("avif", width, height)
+	if crf == "" {
+		crf = "31"
+	}
+	cmd := exec.Command("ffmpeg", "-y", "-i", in.FullPath, "-c:v", "libaom-av1", "-crf", crf, "-still-picture", "1", out)
 	log.Printf("ffmpeg生成的命令:%v\n", cmd.String())
 	if err := util.ExecCommand(cmd, in.FullPath); err != nil {
 		log.Fatalf("ffmpeg命令%s运行中产生错误:%v\n", cmd.String(), err)
