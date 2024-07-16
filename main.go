8ff3293ba9fef096d0d7dc8b8891ae18139ec811
zen <zhangyiming748@gmail.com>
2024-07-16 17:22:43 +0800

    使用最基础的方法读取文件列表

--- main.go
+++ main.go
@@ -1,43 +1,32 @@
 package main
 
 import (
+	"github.com/schollz/progressbar/v3"
 	"github.com/zhangyiming748/ConvertImage/constant"
 	"github.com/zhangyiming748/ConvertImage/conv"
 	mylog "github.com/zhangyiming748/ConvertImage/log"
 	"github.com/zhangyiming748/ConvertImage/util"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 func init() {
 	mylog.SetLog()
 }
 func main() {
+	//os.Setenv("root", "/mnt/d/images")
 	if root := os.Getenv("root"); root == "" {
 		log.Printf("$root为空,使用默认值:%v\n", constant.GetRoot())
 	} else {
 		constant.SetRoot(root)
 		log.Printf("$root不为空:%v\n", constant.GetRoot())
 	}
-	err := filepath.Walk(constant.GetRoot(), func(p string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			absPath, err := filepath.Abs(p)
-			if err != nil {
-				return err
-			}
-			log.Printf("准备处理的文件夹:%v\n", info.Name())
-			files := util.GetAllFiles(absPath)
-			for _, file := range files {
-				conv.ProcessImage(*util.GetBasicInfo(file))
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		log.Println("Error:", err)
+	files := util.GetAllFiles(constant.GetRoot())
+	length := len(files)
+	bar := progressbar.New(length)
+	defer bar.Finish()
+	for i, file := range files {
+		bar.Set(i)
+		conv.ProcessImage(file)
 	}
 }
